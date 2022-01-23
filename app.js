c7ab3f94cd180ead972a3a545fa84f8b97b4390c
Jakob Moritz <jakob@moritz.com>
2022-01-23 14:28:16 +0100

    toll toll toll, ein erster commit: Basic hello world

---
+++ app.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello World, deployed from GitHub!"));
+app.listen(3000, () => console.log("Server ready"));
