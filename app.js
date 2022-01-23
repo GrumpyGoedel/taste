72af5a7d7c1d83ea10378b9b4892588c7dfb844b
Jakob Moritz <jakob@moritz.com>
2022-01-23 15:24:16 +0100

    ja weiss auch nicht

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World, deployed from GitHub! WTF???"));
+app.get("/", (req, res) => res.send("Hello World, UPDATED and deployed from GitHub!"));
 app.listen(3000, () => console.log("Server ready"));
