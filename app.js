27ac4201443b23c0550146406e220e2f4edccef9
Jakob Moritz <jakob@moritz.com>
2022-01-23 15:12:06 +0100

    Erster richtiger commit Nur was angehaengt um webhook zu testen

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World, deployed from GitHub!"));
+app.get("/", (req, res) => res.send("Hello World, deployed from GitHub! WTF???"));
 app.listen(3000, () => console.log("Server ready"));
