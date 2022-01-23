fd163eebfb9ce1f9c53a1ee4fb14ab9f086d41ab
Jakob Moritz <jakob@moritz.com>
2022-01-23 15:27:54 +0100

    ne nenene

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World, UPDATED and deployed from GitHub!"));
+app.get("/", (req, res) => res.send("Ciao World, UPDATED and deployed from GitHub!"));
 app.listen(3000, () => console.log("Server ready"));
